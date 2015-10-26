5aad3da913215e49dccf26979370b2841fbaee1c
qibinyi <qibinyi@peoplemediumdeMac-mini-3.local>
2015-10-26 17:54:37 +0800

    m

--- src/scene/marchingSquareScene.js
+++ src/scene/marchingSquareScene.js
@@ -1,17 +1,20 @@
 
-function createLoginLayer() {
+
+
+function createMSLayer() {
 	var layer = cc.Layer.create(  )
 
+
+	cc.log( "createMSLayer" )
 	return layer
 
 }
 
 
-
-var loginScene = cc.Scene.extend({
+var marchingSquareScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        var layer = createLoginLayer()
+        var layer = createMSLayer()
         this.addChild(layer);
     }
 });
